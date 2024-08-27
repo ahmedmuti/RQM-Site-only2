49ff253361c8c4caded44fc159ff8b12e5d8604b
ahmedmuti <ahmedmutti@gmail.com>
2024-08-27 04:46:11 +0300

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "mainLinear": "linear-gradient(180deg, #0b204ae7 56.19%, #0b204a 100%) !important",
-        "secondLinear": "linear-gradient(180deg, #0B204A 56.19%, #0B204A 100%) !important"
+        "mainLinear": "linear-gradient(180deg, #0b204ae7 56.19%, #091b40 100%) !important",
+        "secondLinear": "linear-gradient(180deg, #0B204A 56.19%, #0B204A 100%) !important",
+        "darkBodyBg": "linear-gradient(180deg, #091B40 56.19%, #152A54 100%) !important",
+        "darkBodyBg2": "linear-gradient(180deg, #152A54 100%, #152A54 100%) !important",
       },
       colors: {
         "mainColor": "#152A54",
         "secondColor": "#3D5579",
+        "darkSecondColor": "#CBDAF1"
+      },
+      borderColor: {
+        "mainBorder": "#E8E8E8"
       }
     },
   },
