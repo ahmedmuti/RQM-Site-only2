b6cfcf8dcdbe5d250c80167ae7f2cec4e79d033a
ahmedmuti <ahmedmutti@gmail.com>
2024-09-02 03:07:44 +0300

    kj

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "mainLinear": "linear-gradient(180deg, #0b204ae7 56.19%, #091b40 100%) !important",
+        "whoWeAre": "linear-gradient(180deg, #152a54d6 100%, #5BB0B9 -1%), url(../images/who_we_are.svg)",
         "secondLinear": "linear-gradient(180deg, #0B204A 56.19%, #0B204A 100%) !important",
         "darkBodyBg": "linear-gradient(180deg, #091B40 56.19%, #152A54 100%) !important",
         "darkBodyBg2": "linear-gradient(180deg, #152A54 100%, #152A54 100%) !important",
